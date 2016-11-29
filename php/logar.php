5d81b73d40d50fe793a1873128df87e071fbed55
Havallon <havallon.ecomp@gmai.com>
2016-11-28 23:37:27 -0300

    cadastro

--- php/logar.php
+++ php/logar.php
@@ -1,11 +1,6 @@
 <?php
 	include 'mySQL.php';
 	require 'mySQL.php';
-	
-    header("Access-Control-Allow-Origin: *");
-	header("Access-Control-Allow-Headers: Content-Type");
-	header("Content-Type: application/json; charset=UTF-8");
-	
 ?>
 
 <?php
--- php/mySQL.php
+++ php/mySQL.php
@@ -1,4 +1,9 @@
 <?php
+
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Headers: Content-Type");
+	header("Content-Type: application/json; charset=UTF-8");
+	
 	$hostname = "localhost";
 	$username = "root";
 	$password = "lucas123";
