b5b41735687907890e7d7fa986cdcb2dfed72754
lucas <lukspaiva95@gmail.com>
2016-12-01 23:09:58 -0300

    botao de vendas

--- dsOutlet/src/app/pages/vendas/vendas.component.ts
+++ dsOutlet/src/app/pages/vendas/vendas.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-import { Produto } from '../../model/produto';
+import { toast } from 'angular2-materialize';
 import { UserService } from '../../services/user.service';
+import { Produto } from '../../model/produto';
 
 @Component({
   selector: 'app-vendas',
@@ -13,9 +14,12 @@ export class VendasComponent implements OnInit {
       private isLogado: boolean = false;
       private isAdmin: boolean = false;
 
-      vendaFinalizada: boolean = true;
+      private vendaFinalizada: boolean;
       marcas: string[] = ['marca1', 'marca2', 'marca3'];
       modelos: string[] = ['modelo1', 'modelo2', 'modelo3'];
+
+      produtos: any[] = [{nome: 'teste', preco: 20}, {nome: 'teste', preco: 20}, {nome: 'teste', preco: 20}, {nome: 'teste', preco: 20}];
+      valorTotal: number = 0;
 
       constructor(private router: Router, private userService: UserService) {
         let stats = this.userService.userStats();
@@ -27,8 +31,19 @@ export class VendasComponent implements OnInit {
         /*se os dados indicarem que usuario não está logado, ele será redirecionado para a pagina principal*/
         if (!this.isLogado) {
           this.router.navigate(['/home']);
-        } else {
         }
+      }
+
+      private adicionarItem(){
+        toast('Produto adicionado!', 2000, 'rounded');
+        for (let item of this.produtos) {
+          console.log(item); // 1, "string", false
+          this.valorTotal = this.valorTotal + item.preco;
+        }
+      }
+
+      private finalizar(){
+        this.vendaFinalizada = true;
       }
 
     }
