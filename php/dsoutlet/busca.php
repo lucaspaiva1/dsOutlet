2e2795346a65f1e0d8f7227fbaccc6f166560b26
Havallon <havallon.ecomp@gmai.com>
2016-12-02 23:43:47 -0300

    buscando cliente

--- php/dsoutlet/busca.php
+++ php/dsoutlet/busca.php
@@ -5,7 +5,7 @@
 
 <?php 
 
-	$vetor = array();
+	$vetor   = array();
 		$the_request = &$_GET;
 		if (isset($_GET["id"])){
 			if ($_GET["id"] == ""){
@@ -33,6 +33,23 @@
 					$vetor[] = $row;
 				}
 				echo json_encode($vetor);
+			} else {
+				$id = $_GET['id'];
+				$sql = "SELECT * FROM cliente WHERE id = '$id'";
+				$result = $con->query($sql);
+				
+				$numrow = $result->num_rows;
+				if($numrow !== 1){
+					echo json_encode(false);
+				}else{
+					$dados = $result->fetch_assoc();
+					$endID = $dados['enderecoIDEndereco'];
+					$sql = "SELECT * FROM endereco WHERE id = '$endID'";
+					$result = $con->query($sql);
+					$end = $result->fetch_assoc();
+					array_push($vetor, $dados, $end);
+					echo json_encode($vetor);
+				}	
 			}
 		}
 	
