74f1c19ef567cc8142d7e8131d93247552838167
cleybson cardoso <cleybsonc@gmail.com>
2016-12-06 02:06:37 -0200

    iniciando tela de vendas

--- dsOutlet/src/app/pages/vendas/vendas.component.ts
+++ dsOutlet/src/app/pages/vendas/vendas.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { toast } from 'angular2-materialize';
 import { UserService } from '../../services/user.service';
+import { ProdutosService } from '../../services/produtos.service';
 import { Produto } from '../../model/produto';
 
 @Component({
@@ -11,39 +12,45 @@ import { Produto } from '../../model/produto';
 })
 export class VendasComponent implements OnInit {
 
-      private isLogado: boolean = false;
-      private isAdmin: boolean = false;
+  private isLogado: boolean = false;
+  private isAdmin: boolean = false;
+  private vendaFinalizada: boolean;
+  private produtos: Produto[] = [];
+  private itens: Produto[]=[]; 
+  valorTotal: number = 0;
+  private search: string;
 
-      private vendaFinalizada: boolean;
-      marcas: string[] = ['marca1', 'marca2', 'marca3'];
-      modelos: string[] = ['modelo1', 'modelo2', 'modelo3'];
+  constructor(private router: Router, private userService: UserService, private produtosService: ProdutosService) {
+    let stats = this.userService.userStats();
+    this.isLogado = stats[0];
+    this.isAdmin = stats[1];
+    this.produtosService.getProdutos().then(res => {
+      this.produtos = res;
+    });
+    this.search="";
+  }
 
-      produtos: any[] = [{nome: 'teste', preco: 20}, {nome: 'teste', preco: 20}, {nome: 'teste', preco: 20}, {nome: 'teste', preco: 20}];
-      valorTotal: number = 0;
-
-      constructor(private router: Router, private userService: UserService) {
-        let stats = this.userService.userStats();
-        this.isLogado = stats[0];
-        this.isAdmin = stats[1];
-      }
-
-      ngOnInit() {
-        /*se os dados indicarem que usuario não está logado, ele será redirecionado para a pagina principal*/
-        if (!this.isLogado) {
-          this.router.navigate(['/home']);
-        }
-      }
-
-      private adicionarItem(){
-        toast('Produto adicionado!', 2000, 'rounded');
-        for (let item of this.produtos) {
-          console.log(item); // 1, "string", false
-          this.valorTotal = this.valorTotal + item.preco;
-        }
-      }
-
-      private finalizar(){
-        this.vendaFinalizada = true;
-      }
-
+  ngOnInit() {
+    /*se os dados indicarem que usuario não está logado, ele será redirecionado para a pagina principal*/
+    if (!this.isLogado) {
+      this.router.navigate(['/home']);
     }
+  }
+
+  private adicionarItem() {
+    toast('Produto adicionado!', 2000, 'rounded');
+    for (let item of this.produtos) {
+      console.log(item); // 1, "string", false
+      this.valorTotal = this.valorTotal + item.precoSaidaPadrao;
+    }
+  }
+
+  private finalizar() {
+    this.vendaFinalizada = true;
+  }
+
+  limpar(){
+    this.search="";
+  }
+
+}
