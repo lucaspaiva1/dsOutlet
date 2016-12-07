6dce04f3175818e0d72699155b5b8611f5a47b77
cleybson cardoso <cleybsonc@gmail.com>
2016-12-07 00:17:17 -0200

    atual

--- dsOutlet/src/app/pages/vendas/vendas.component.ts
+++ dsOutlet/src/app/pages/vendas/vendas.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { toast } from 'angular2-materialize';
 import { UserService } from '../../services/user.service';
 import { ProdutosService } from '../../services/produtos.service';
 import { Produto } from '../../model/produto';
 import { Divida } from '../../model/linhaDeitem';
+import { MaterializeAction } from 'angular2-materialize';
 
 @Component({
   selector: 'app-vendas',
@@ -15,7 +16,6 @@ export class VendasComponent implements OnInit {
 
   private isLogado: boolean = false;
   private isAdmin: boolean = false;
-  private vendaFinalizada: boolean;
   private produtos: Produto[] = [];
   private itens: Produto[] = [];
   valorTotal: number = 0;
@@ -25,8 +25,11 @@ export class VendasComponent implements OnInit {
   private compra: Divida[] = [];
   private quantidade: number = 0;
   private permitirCompra: boolean = false;
+  modalActions = new EventEmitter<string | MaterializeAction>();
+
 
   constructor(private router: Router, private userService: UserService, private produtosService: ProdutosService) {
+    this.modalActions.emit({action:"modal",params:['close']});
     let stats = this.userService.userStats();
     this.isLogado = stats[0];
     this.isAdmin = stats[1];
@@ -61,10 +64,6 @@ export class VendasComponent implements OnInit {
     this.itemSelecionado = new Produto();
     this.quantidade = 0;
     this.permitirCompra = false;
-  }
-
-  private finalizar() {
-    this.vendaFinalizada = true;
   }
 
   limpar() {
