9aa7c595fc0fd545d46d2576a5b99231264f55cc
cleybson cardoso <cleybsonc@gmail.com>
2016-12-08 15:23:39 -0200

    php

--- php/dsoutlet/mySQL.php
+++ php/dsoutlet/mySQL.php
@@ -5,9 +5,9 @@
 	header("Access-Control-Allow-Headers: Content-Type");
 	header("Content-Type: application/json; charset=UTF-8");
 
-	$hostname = "localhost";
+	$hostname = "localhost:3307";
 	$username = "root";
-	$password = "lucas123";
+	$password = "cley";
 	$database = "dsoutlet";
 	//conectando ao banco
 	$con = mysqli_connect($hostname, $username, $password, $database) or die (mysqli_error());
