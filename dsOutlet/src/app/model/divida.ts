b8785271232b2775ba177245e7e171040030b55e
cleybson cardoso <cleybsonc@gmail.com>
2016-12-10 00:03:01 -0200

    iniciando parte de dividas

--- dsOutlet/src/app/model/divida.ts
+++ dsOutlet/src/app/model/divida.ts
@@ -6,10 +6,13 @@ export class Divida{
   vencimento: Date;
   dataCompra: Date;
   valorPorParcela: number;
+  valorRecedido: number;
+
 
   constructor(){
     this.valor =0;
     this.parcelasAPagar = 1;
     this.tipoVenda="";
+    this.valorRecedido =0;
   }
 }
