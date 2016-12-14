3b1fd74bdc3d0fe4a2310d6399f9b8f86bad61f7
lucas <lukspaiva95@gmail.com>
2016-12-14 00:59:35 -0300

    teste storage

--- dsOutlet/src/app/pages/home-page/home-page.component.ts
+++ dsOutlet/src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { LocalStorageService } from 'angular-2-local-storage';
 import { UserService } from '../../services/user.service';
 
 @Component({
@@ -10,10 +11,27 @@ export class HomePageComponent {
 
   private isLogado: boolean;
   private isAdmin: boolean;
+  private variavel: any;
+  private error: any;
 
-  constructor(private userService: UserService) {
+  constructor(private storage: LocalStorageService, private userService: UserService) {
     let stats = this.userService.userStats();
     this.isLogado = stats[0];
     this.isAdmin = stats[1];
+
+    this.storage.set('user', 'usuario');
+
+    this.teste();
+
   }
+
+  teste(){
+    try{
+      this.variavel = this.storage.get('user');
+    }catch(e){
+      this.error = e;
+      this.variavel = 'teste';
+    }
+  }
+
 }
