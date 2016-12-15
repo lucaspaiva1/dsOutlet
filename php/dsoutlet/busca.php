17e2f92369828ca1e8c82d8f43f7ad08ccbbe28d
Havallon <havallon.ecomp@gmai.com>
2016-12-14 23:44:17 -0300

    fixed

--- php/dsoutlet/busca.php
+++ php/dsoutlet/busca.php
@@ -120,6 +120,30 @@
 				array_push($dados, $venda, $estoque);
 				echo json_encode($dados);
 			}
+		} else if (isset($_GET['notificacoes'])){
+			if ($_GET['notificacoes'] == ""){
+				$sql = "SELECT c.id, c.nome, d.vencimento, d.valor, d.parcelasApagar from divida d, cliente c where d.cliente_IDCliente = c.id and d.vencimento = curdate()";
+				$result = $con->query($sql);
+				$divida = array();
+				while ($row=$result->fetch_assoc()){
+					$value = $row['valor'];
+					$p     = $row['parcelasApagar'];
+					$x = $value / $p;
+					$row['valorParcela'] = $x;
+					$divida[] = $row;
+				}
+				$sql = "SELECT marca, modelo, tamanho, quantidade, minimo from produto where quantidade <= minimo";
+				$result = $con->query($sql);
+				$estoque = array();
+				while ($row=$result->fetch_assoc()){
+					$row['estado'] = "Falta";
+					$estoque[] = $row;
+				}
+				$dados = array();
+				array_push($dados, $divida, $estoque);
+				
+				echo json_encode($dados);
+			}
 		}
 	
 ?>
