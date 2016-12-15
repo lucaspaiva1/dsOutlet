8a7816dfd3ed31f4ae8c2238ee2eed8a597db07b
Havallon <havallon.ecomp@gmai.com>
2016-12-15 18:19:57 -0300

    fix

--- php/dsoutlet/mySQL.php
+++ php/dsoutlet/mySQL.php
@@ -1,6 +1,6 @@
 <?php
 	
-	
+	date_default_timezone_set('America/Bahia');
     header("Access-Control-Allow-Origin: *");
 	header("Access-Control-Allow-Headers: Content-Type");
 	header("Content-Type: application/json; charset=UTF-8");
